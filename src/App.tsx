df7262271e92f960ad636949e0f92dc3bff2a7c5
ZidaneDanukusuma514 <56733493+ZidaneDanukusuma514@users.noreply.github.com>
2023-02-01 13:16:38 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import About from "./pages/About";
+import Home from "./pages/Home";
+import Nofound from "./pages/Notfound";
+
+export interface IAppProps {}
+
+export default function App(props: IAppProps) {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Nofound />} />
+      </Routes>
+    </Router>
+  );
+}
---
+++ src/pages/Notfound/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+export interface INofoundProps {}
+
+export default function Nofound(props: INofoundProps) {
+  return <div></div>;
+}
