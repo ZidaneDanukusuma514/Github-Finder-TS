901962333939369d644172feb7313a447249b3bd
ZidaneDanukusuma514 <56733493+ZidaneDanukusuma514@users.noreply.github.com>
2023-02-03 21:20:34 +0700

    add token

--- src/component/Header/index.tsx
+++ src/component/Header/index.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 export interface IHeaderProps {}
 export default function Header(props: IHeaderProps) {
   return (
     <header className="bg-indigo-900 py-4">
-      <h1 className="text-2xl text-white uppercase text-center font-sans font-bold">
-        github finder
-      </h1>
+      <Link to={"/"}>
+        <h1 className="text-2xl text-white uppercase text-center font-sans font-bold">
+          github finder
+        </h1>
+      </Link>
     </header>
   );
 }
--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,10 +1,18 @@
 import axios from "axios";
 import * as React from "react";
 export interface IAboutProps {}
-
+import loading from "../../assets/loading.gif";
 export default function About(props: IAboutProps) {
-  const [user, setUser] = React.useState<any>();
-  const [repos, setRepos] = React.useState<any>([]);
+  const [user, setUser] = React.useState<any>(null);
+  const [repos, setRepos] = React.useState<any>(null);
+
+  const Github = axios.create({
+    baseURL: "https://api.github.com/",
+    headers: {
+      Authorization: `token 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`,
+    },
+  });
+
   const handleData = async () => {
     await axios
       .get("https://api.github.com/users/ZidaneDanukusuma514")
@@ -29,28 +37,31 @@ export default function About(props: IAboutProps) {
         <div className="flex flex-col border-2">
           <img
             className="max-w-sm self-center"
-            src={!user ? null : user.avatar_url}
+            src={!user ? loading : user.avatar_url}
             alt=""
           />
+
           <h1 className="text-lg font-semibold text-white">
             Account Name: {!user ? null : user.login}
           </h1>
         </div>
         <div className="col-span-2">
-          <h2 className="text-white text-lg font-bold">My Repo:</h2>
-          <div className="grid grid-cols-4 gap-2 content-center ">
+          <h2 className="text-white text-lg font-bold ">My Repos:</h2>
+          <div className="grid grid-cols-4 gap-2 mt-6 content-center ">
             {!repos ? (
-              <p>No data send</p>
+              <img src={loading} className="mx-auto" alt="" />
             ) : (
               repos.map((item: any, index: number): any => (
                 <div
                   className="flex flex-col p-2 text-white bg-indigo-400 space-y-2"
                   key={index}
                 >
-                  <h1 className="text-white font-semibold">{item.name}</h1>
-                  <p className=" text-black">
-                    {item.description ? item.description : "No Desc"}
-                  </p>
+                  <a href={item.html_url}>
+                    <h1 className="text-white font-semibold">{item.name}</h1>
+                    <p className=" text-black">
+                      {item.description ? item.description : "No Desc"}
+                    </p>
+                  </a>
                 </div>
               ))
             )}
